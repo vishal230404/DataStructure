b63d2530ec6fc7a289d77b8d815d8e275095c5b0
Vishal vishal <ce220004050@iiti.ac.in>
2024-12-01 11:36:33 +0530

    trie Implimentation

---
+++ Trie/trie1.cpp
@@ -0,0 +1,41 @@
+#include <bits/stdc++.h>
+using namespace std;
+struct trie {
+	trie *next[26];
+	trie() {
+		for(int i=0; i<26; i++) {
+			next[i]=NULL;
+		}
+	}
+	bool ended=false;
+};
+void insert(string s,trie *root) {
+	trie *curr=root;
+	for(int i=0; i<(int)s.size(); i++) {
+	    int idx=s[i]-'a';
+		if(curr->next[idx]==NULL) {
+			curr->next[idx]=new trie();
+			curr=curr->next[idx];
+		}
+	}
+	curr->ended=true;
+}
+bool in_trie(string s,trie *root) {
+	trie *curr=root;
+	
+	for(int i=0; i<s.size(); i++) {
+	    int idx=s[i]-'a';
+		if(curr->next[idx]==NULL) return false;
+		curr=curr->next[idx];
+	}
+	return curr->ended;
+}
+int main()
+{
+	trie *root=new trie();
+	insert("vishal",root);
+	insert("apaar",root);
+	cout<<in_trie("apar",root);
+
+	return 0;
+}
